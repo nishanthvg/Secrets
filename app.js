03ef9451df34acbba365d328de71de97dd56c93e
nishanthvg <60750619+nishanthvg@users.noreply.github.com>
2020-06-30 16:08:11 +0530

    Replace encryptoin with hash

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require("express");
 const ejs = require("ejs");
 const mongoose = require("mongoose")
 const encrypt = require("mongoose-encryption");
+var md5 = require('md5');
 
 const app = express();
 
@@ -18,8 +19,6 @@ const userSchema = new mongoose.Schema( {
     password:String
 });
 
-//Password encryption 
-userSchema.plugin(encrypt, { secret: process.env.SCERET, encryptedFields: ['password'] });
 
 
 const User = mongoose.model("User",userSchema);
@@ -35,7 +34,7 @@ app.route("/login")
     })
     .post((req,res) => {
         const username = req.body.username;
-        const password = req.body.password;
+        const password = md5(req.body.password);
         User.findOne({email: username},(err,foundUser) =>{
             if(!err) {
                 if(foundUser.password === password) {
@@ -56,7 +55,7 @@ app.route("/register")
     .post((req,res) => {
         const newUser = new User({
             email:req.body.username,
-            password:req.body.password
+            password:md5(req.body.password)
         });
         newUser.save((err) => {
             if(err) {
