9f710a910c78c21914173c9cf6cbe636ef4c4310
nishanthvg <60750619+nishanthvg@users.noreply.github.com>
2020-06-30 13:35:37 +0530

    App.js

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-//jshint esversion:6
+const express = require("express");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.static("public"));
+app.set('view engine',ejs);
+
+
+app.listen(3000,() => [
+    console.log("Server runs in port 3000")
+])
