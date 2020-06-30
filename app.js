14e701c45d0ea697d16067ed6460314164e359a8
nishanthvg <60750619+nishanthvg@users.noreply.github.com>
2020-06-30 14:08:58 +0530

    added routes get,post requests

--- app.js
+++ app.js
@@ -1,11 +1,65 @@
 const express = require("express");
 const ejs = require("ejs");
+const mongoose = require("mongoose")
 
 const app = express();
 
 app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
-app.set('view engine',ejs);
+app.set('view engine',"ejs");
+
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true,useUnifiedTopology:true})
+
+const userSchema = {
+    email:String,
+    password:String
+}
+
+const User = mongoose.model("User",userSchema);
+
+app.get("/",(req,res) => {
+    res.render("home");
+});
+
+
+app.route("/login")
+    .get((req,res) => {
+        res.render("login");
+    })
+    .post((req,res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+        User.findOne({email: username, password:password},(err,foundUser) =>{
+            if(!err) {
+                if(foundUser) {
+                    res.render("secrets");
+                } else {
+                    res.render("login")
+                }
+            }
+        });
+    });
+
+
+app.route("/register")
+    .get((req,res) => {
+        res.render("register");
+    })
+
+    .post((req,res) => {
+        const newUser = new User({
+            email:req.body.username,
+            password:req.body.password
+        });
+        newUser.save((err) => {
+            if(err) {
+                console.log(err)
+            } else {
+                res.render("secrets")
+            }
+        });
+    });
+
 
 
 app.listen(3000,() => [
